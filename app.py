6e3f450e552d8189167b72656346b391fa21163a
shaktivanani <arjansambad@gmail.com>
2025-03-14 11:17:58 +0530

    portfoliyo update code

--- app.py
+++ app.py
@@ -9,8 +9,12 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-   return render_template("index.html",)
+def index():
+   return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 if __name__ =="__main__":
